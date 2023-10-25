8a171bdcbdcc177815f007a1c6beca634a665f7e
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-25 20:36:14 +0200

    CarDto - w pakiecie dto - do getterów i setterów

---
+++ src/main/java/com/example/demo9/dto/CarDto.java
@@ -0,0 +1,14 @@
+package com.example.demo9.dto;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class CarDto {
+    private String brand;
+    private String model;
+    private String color;
+}
