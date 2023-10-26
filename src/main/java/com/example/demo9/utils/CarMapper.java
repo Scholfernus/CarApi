e6395db81909b55ba851295a7e7fd195c1f83e6c
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-26 22:34:28 +0200

    CarMapper - nowa klasa

---
+++ src/main/java/com/example/demo9/utils/CarMapper.java
@@ -0,0 +1,21 @@
+package com.example.demo9.utils;
+
+import com.example.demo9.dto.CarDto;
+import com.example.demo9.model.CarModel;
+
+public class CarMapper {
+public static CarModel toCarModel(CarDto carDto){
+    CarModel carModel = new CarModel();
+    carModel.setBrand(carDto.getBrand());
+    carModel.setModel(carDto.getModel());
+    carModel.setColor(carDto.getColor());
+    return carModel;
+}
+public static CarDto toCarDto(CarModel carModel){
+    CarDto carDto = new CarDto();
+    carDto.setBrand(carModel.getBrand());
+    carDto.setModel(carModel.getModel());
+    carDto.setColor(carModel.getColor());
+    return carDto;
+}
+}
