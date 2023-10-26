90c0d8417f275f72e11fa08ec0eeff3d3ddd7b93
Scholfernus <48571340+Scholfernus@users.noreply.github.com>
2023-10-26 22:38:16 +0200

    data.sql - baza startowa

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO car_model(brand, model, color)
+VALUES ('Audi','A8','Red');
+
+INSERT INTO car_model(brand, model, color)
+VALUES ('Ferrari','Node','Yellow');
+
+INSERT INTO car_model(brand, model, color)
+VALUES ('Skoda','Codiaq','Black');
